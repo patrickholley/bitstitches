8f6bbb145b9488e2ba807548aac63f0e73681c5a
Patrick Holley <patrick_holley@outlook.com>
2019-01-02 21:17:27 -0700

    Some initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Bits Get Stitches
+            </div>
+        )
+    }
+}
+
+export default App;
