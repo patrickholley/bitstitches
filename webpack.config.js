b934f3a7504d5120cdbd46fb8423d4e8dd2a99b5
Patrick Holley <patrick_holley@outlook.com>
2019-03-11 20:42:05 -0600

    added fonts

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(ttf)/,
+        use: ['file-loader'],
+      },
     ],
   },
   plugins: [
